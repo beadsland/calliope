eb07ebff9ceb7a1e585c2187645d721531b74c79
beadsland <beads.d.land@gmail.com>
2020-07-31 03:36:06 -0400

    hatchet job attempting to restore 3 weeks of lost functionality

--- app/scripts/inline/doubletake.js
+++ app/scripts/inline/doubletake.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var swipeLeft = function() {
+var swipeLeft = function(reflink) {
+  document.title = document.title.replace(/^.*—/u, "");
   var passcoll = document.getElementsByClassName("doubletake-pass-button");
   if (passcoll.length) {
     passcoll[0].click();
@@ -8,70 +9,133 @@ var swipeLeft = function() {
       alreadyWordCounts().remove();
       setTimeout(function() { parsePage() }, 500 + Math.random()*500);
     } else {
-      setTimeout(function() { checkStacks() }, 500);
+      setTimeout(function() { selectProfile() }, 500);
     }
   }
 }
 
-var handlePage = function(wc, fc) {
-  console.log("handling...");
-  if ( (wc-fc < 150) || (fc/wc > 0.08) ) {
-    setTimeout(function() { swipeLeft() }, 500);
+var badNoiseRatio = function(wc, fc) {
+  var location = document.getElementsByClassName("cardsummary-location");
+  if (location.length) {
+    location = location[0].innerText;
+    if (location == "Manhattan, NY") { return (wc-fc < 50 || fc/wc > 0.20) }
+    if (location == "Brooklyn, NY") { return (wc-fc < 100 || fc/wc > 0.15) }
+    if (location == "Philadephia, PA") { return (wc-fc < 150 || fc/wc > 0.125) }
+    if (location == "Conshohocken, PA") { return (wc-fc < 150 || fc/wc > 0.11) }
+    if (location.includes(", NJ")) { return (wc-fc < 150 || fc/wc > 0.10) }
+  }
+  return (wc-fc < 150 || fc/wc > 0.09);
+}
+
+var checkLeftPage = function(reflink) {
+  console.log("awaiting departure...");
+  if (document.location.href.includes("profile")) {
+    setTimeout(function() { checkLeftPage(reflink) }, 2000);
   } else {
-    var linkcoll = document.getElementsByClassName("cardsummary-profile-link");
-    if (linkcoll.length) {
-      window.location.href = linkcoll[0].getElementsByTagName("a")[0].href;
+    selectProfile();
+  }
+}
+
+var checkEye = function() {
+  var essays = getEssays();
+  if (essays.length) {
+    document.title = "👁 " + document.title.replace("👁 ", "");
+  } else {
+    setTimeout(function() { checkEye() }, 1000);
+  }
+}
+
+var handlePage = function(wc, fc, reflink, thislink) {
+  console.log("handling...");
+  if (!thislink) {
+    document.title = "👁 " + document.title.replace("👁 ", "");
+    setTimeout(function() { checkLeftPage(reflink) }, 2000);
+  }
+  if ( badNoiseRatio(wc, fc) ) {
+    setTimeout(function() { swipeLeft(reflink) }, 500);
+  } else {
+    if (thislink) {
+      window.location.href = thislink;
     } else {
-      setTimeout(function() { checkStacks() }, 500);
+      checkLeftPage(reflink);
     }
   }
 }
 
-var parsePage = function(essays) {
-  console.log("parsing...");
-  var wc = getEssayWordCounts(essays);
-  if (typeof wc !== 'undefined') {
-    setTimeout(function() { handlePage(wc[0], wc[1]) }, 500);
+var parsePage = function(essays, reflink, n) {
+  window.scrollTo(0, 650);
+
+  var realname = document.getElementsByClassName("cardsummary-realname");
+  if (realname.length) {
+    document.title = realname[0].innerText + "—" + document.title.replace(/^.*—/, "");
+  } else {
+    setTimeout(function() { checkEye() }, 2000);
+    window.scrollTo(0, 250);
+    document.getElementsByClassName("profile-essays-expander")[0].click()
+  }
+
+  var linkcoll = document.getElementsByClassName("cardsummary-profile-link");
+  var thislink = "";
+  if (linkcoll.length) {
+    thislink = linkcoll[0].getElementsByTagName("a")[0].href;
+  }
+  console.log(thislink + " v. " + reflink);
+  if (thislink == reflink) { document.location = "http://okcupid.com"; }
+  else {
+    if (!reflink && !n) { reflink = thislink; }
+
+    console.log("parsing...");
+    if (!essays) { essays = getEssays() }
+    var wc = getEssayWordCounts(essays);
+    if (typeof wc !== 'undefined') {
+      setTimeout(function() { handlePage(wc[0], wc[1], reflink, thislink) }, 500);
+    } else {
+      setTimeout(function() { parsePage(essays, reflink, n) }, 500);
+    }
   }
 }
 
-var loadStacks = function(stacks, n) {
+var loadStacks = function(stacks, n, reflink) {
   if (!document.getElementsByClassName("stacks-title-bar").length) {
-    visitStacks(stacks, n-1);
+    visitStacks(stacks, n-1, reflink);
   } else {
     if (document.getElementsByClassName("stacks-paywall").length) {
-      visitStacks(stacks, n-1);
+      visitStacks(stacks, n-1, reflink);
     }
   }
-  setTimeout(function() { parsePage(getEssays()); }, 1000);
+  setTimeout(function() {
+    parsePage(getEssays(), reflink, n); }, 1000
+  );
 }
 
-var visitStacks = function(stacks, n) {
+var visitStacks = function(stacks, n, reflink) {
   console.log("visiting...");
-  console.log(stacks.length);
+  window.scrollTo(0, 650);
   stacks[n].click();
-  setTimeout(function() { loadStacks(stacks, n); }, 500);
+  setTimeout(function() { loadStacks(stacks, n, reflink); }, 500);
 }
 
-var checkStacks = function() {
+var checkStacks = function(reflink) {
   console.log("checking...");
+  window.scrollTo(0, 650);
   var stacks = document.getElementsByClassName("stacks-menu-item");
   if (stacks.length) {
-    setTimeout(function() { visitStacks(stacks, stacks.length-1); }, 1000);
+    setTimeout(function() { visitStacks(stacks, stacks.length-1, reflink); }, 1000);
   }
-  else { setTimeout(function() { checkStacks() }, 5000); }
+  else { setTimeout(function() { selectProfile(reflink) }, 5000); }
 }
 
 var waitProfile = function() {
   console.log("waiting...");
+  window.scrollTo(0, 650);
   var essays = getEssays();
   if (!essays.length) { setTimeout(function() { waitProfile() }, 3000); }
   else { setTimeout(function() { parsePage(essays); }, 1000); }
 }
 
-var selectProfile = function() {
+var selectProfile = function(reflink) {
   console.log("selecting...");
-  if (window.location.href.indexOf("profile") == -1) { checkStacks(); }
+  if (!window.location.href.includes("profile")) { checkStacks(reflink); }
   else { waitProfile(); }
 }
 
@@ -79,10 +143,12 @@ var checkReady = function() {
   if (alreadyWordCounts()) {
     setTimeout(function() { checkReady() }, 10000);
   } else {
-    console.log("loading...");
-    if (document.readyState == "complete"  &&
-       document.location.href.includes("okcupid.com")) { selectProfile(); }
-    else { setTimeout(function() { checkReady() }, 3000); }
+    if (document.location.href.includes("okcupid.com")) {
+      console.log("loading okcupid...");
+      window.scrollTo(0, 650);
+      if (document.readyState == "complete") { selectProfile(); }
+      else { setTimeout(function() { checkReady() }, 3000); }
+    }
   }
 }
 
