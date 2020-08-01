85427db25d28dae5455ed60f120d9810245a50b1
beadsland <beads.d.land@gmail.com>
2020-08-01 08:11:03 -0400

    clean up

--- lib/okcupid.ex
+++ lib/okcupid.ex
@@ -4,18 +4,13 @@ defmodule OkCupid do
   """
 
   def start(), do: heartbeat(:document.location.pathname)
-  def log(obj), do: :console.log(obj)
 
   def heartbeat("/home"), do: doubletake()
   def heartbeat("/doubletake"), do: doubletake()
   def heartbeat("/profile"), do: profile(:mine)
   def heartbeat("/profile/" <> _user), do: profile(:page)
   def heartbeat("/"), do: :ignore
-  def heartbeat(str) do
-    # String.slice throws a FunctionClauseError, so use equivalent javasript
-    path = :text.slice(0, -1)
-    heartbeat(path, str.last)
-  end
+  def heartbeat(str), do: heartbeat(str.substring(0, str.length()-1), str.last)
 
   def heartbeat(str, "/"), do: heartbeat(str)
   def heartbeat(_str, _), do: :ignore
@@ -23,8 +18,8 @@ defmodule OkCupid do
   def doubletake(), do: doubletake(Page.getClass("cardsummary-profile-link"))
   def doubletake([elem | _tail]), do: doubletake({:anchor, DOM.getTag(elem, "a")})
   def doubletake({:anchor, [elem | _tail]}), do: doubletake({:href, elem.href})
-  def doubletake({:href, href}), do: log(href)
-  def doubletake(err), do: log([:doubletake, :err, err])
+  def doubletake({:href, href}), do: :console.log(:profile, href)
+  def doubletake(err), do: :console.log([:doubletake, :err, err])
 
   def profile(_), do: :ignore
 
